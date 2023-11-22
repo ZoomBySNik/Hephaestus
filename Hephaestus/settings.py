eda1d34360d7aee11ba54a773cb9f77fd389e038
ZoomBySNik <Onyx1504@yandex.ru>
2023-11-22 17:57:39 +0300

    Создает модель пользователя

--- Hephaestus/settings.py
+++ Hephaestus/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
+    'domain',
 ]
 
 MIDDLEWARE = [
@@ -99,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL = 'domain.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- domain/models.py
+++ domain/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class User(AbstractUser):
+    patronymic = models.CharField(max_length=255, blank=False, null=False, verbose_name='Отчество')
+    phone_number = PhoneNumberField(region="RU", blank=False, null=False, verbose_name='Номер телефона')
+    profile_photo = models.ImageField(blank=True, null=True, verbose_name='Фото профиля', upload_to='user_photos')
+
+    def __str__(self):
+        return '%s %s %s' % (self.first_name, self.patronymic, self.last_name)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
