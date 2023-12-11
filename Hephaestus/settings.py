75c026da4cd5ab8c82d4db179ca8c5f3e0b4a953
ZoomBySNik <Onyx1504@yandex.ru>
2023-12-11 19:44:34 +0300

    Создает навбар на главной странице и много всяческих html файлов

--- Hephaestus/settings.py
+++ Hephaestus/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Hephaestus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'domain/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
