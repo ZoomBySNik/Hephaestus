3ead1a2413e8f90cdf2dcab359b51aa3dcd95110
ZoomBySNik <Onyx1504@yandex.ru>
2023-12-12 17:40:53 +0300

    Подключает админку, без настроек

--- domain/admin.py
+++ domain/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.hashers import make_password
-
+from django.apps import apps
 from domain.models import CustomUser
+
+# Register your models here.
+app_config = apps.get_app_config('domain')
 
 
 class CustomInAdminUser(CustomUser):
@@ -14,5 +17,10 @@ class CustomInAdminUser(CustomUser):
         super().save_model(request, obj, form, change)
 
 
+# Те модели которые не нужно регистрировать!!! Их нужно импортнуть отдельно
+NOT_REGISTERED_MODELS = [CustomInAdminUser, CustomUser]
+for model in app_config.get_models():
+    if not (model in NOT_REGISTERED_MODELS):
+        admin.site.register(model)
 # Register your models here.
 admin.site.register(CustomInAdminUser)
