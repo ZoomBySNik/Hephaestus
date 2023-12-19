f7ac525d384fa8004722920fb8f291a7ad72a7d4
ZoomBySNik <Onyx1504@yandex.ru>
2023-12-19 12:43:39 +0300

    Реализует переключатель для парных блоков

--- domain/forms.py
+++ domain/forms.py
@@ -16,6 +16,8 @@ class EmployerForm(forms.ModelForm):
 
 
 class OrganizationForm(forms.ModelForm):
+    position = forms.CharField(max_length=90, label='Должность',
+                               error_messages={"required": "Введите должность"})
     locality = forms.CharField(max_length=255, label='Населенный пункт',
                                error_messages={"required": "Введите населённый пункт"})
     street = forms.CharField(max_length=255, label='Улица',
@@ -23,7 +25,8 @@ class OrganizationForm(forms.ModelForm):
     number_of_building = forms.CharField(max_length=31, label='Номер строения',
                                          error_messages={"required": "Введите номер строения"})
     apartment_number = forms.CharField(max_length=31, label='Номер помещения',
-                                       error_messages={"required": "Введите номер помещения"})
+                                       required=False)
+
     class Meta:
         model = Organization
         exclude = ['address']
