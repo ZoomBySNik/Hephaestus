26fd64c507b42cb1abeb5ec4a6ee65c076a155a9
ZoomBySNik <Onyx1504@yandex.ru>
2023-12-26 21:26:46 +0300

    Реализует вывод создания заявки по логическим блокам и их сохранение в базе

--- domain/forms/job_seekers_forms.py
+++ domain/forms/job_seekers_forms.py
@@ -5,6 +5,13 @@ from domain.models import *
 
 
 class JobSeekerForm(forms.ModelForm):
+    locality = forms.CharField(max_length=255, label='Населенный пункт',
+                               error_messages={"required": "Введите населённый пункт"})
     class Meta:
         model = JobSeeker
         fields = '__all__'
+        exclude = ['address', 'specialization', 'skill', 'about']
+        widgets = {
+            'birthdate': forms.DateInput(attrs={'type': 'date'}),
+            'about': forms.Textarea(attrs={'rows': 4}),
+        }
--- domain/views/job_seeker_views.py
+++ domain/views/job_seeker_views.py
@@ -10,7 +10,12 @@ def job_seeker_create_view(request):
     if request.method == 'POST':
         form = JobSeekerForm(request.POST, request.FILES)
         if form.is_valid():
-            job_seeker = form.save()
+            address = Address.objects.create(
+                locality=form.cleaned_data['locality'],
+            )
+            job_seeker = form.save(commit=False)
+            job_seeker.address = address
+            job_seeker.save()
             return redirect('home')
     else:
         form = JobSeekerForm()
