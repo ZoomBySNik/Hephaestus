64dd1011af05eb5a345b3329f14e0060c603bbb4
ZoomBySNik <Onyx1504@yandex.ru>
2024-05-23 13:32:48 +0300

    Теперь это прогрессивное веб-приложение!

--- domain/context_processors.py
+++ domain/context_processors.py
@@ -1,30 +1,18 @@
 from django.contrib.auth.context_processors import auth
 
 import Hephaestus
+from domain.general_functions import *
 from domain.models import JobSeeker, Employer, Employee
-
-
-def get_user_type(user_id):
-    try:
-        job_seeker = JobSeeker.objects.get(id=user_id)
-        return 'job_seeker'
-    except JobSeeker.DoesNotExist:
-        pass
-
-    try:
-        employer = Employer.objects.get(id=user_id)
-        return 'employer'
-    except Employer.DoesNotExist:
-        pass
-
-    try:
-        employee = Employee.objects.get(id=user_id)
-        return 'employee'
-    except Employee.DoesNotExist:
-        return None
 
 
 def user(request):
     user_id = request.user.id
     user_type = get_user_type(user_id)
-    return {'user': request.user, 'user_type': user_type, 'DADATA_API_KEY': Hephaestus.settings.DADATA_API_KEY}
+    if user_type == 'employee':
+        rus_user_type = Employee.objects.get(id=user_id).employee_position.name
+    else:
+        rus_user_type = get_rus_user_type(user_type)
+    return {'user': request.user,
+            'user_type': user_type,
+            'rus_user_type': rus_user_type,
+            'DADATA_API_KEY': Hephaestus.settings.DADATA_API_KEY}
